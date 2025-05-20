30a2824f29b0b2f89e34ae783064a8b5d02ff2c3
diana <mirceadiana15@gmail.com>
2025-05-20 13:08:42 -0600

    created player and circle class for the sprites

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 # importing all the constants
 from constants import *
+from player import Player
 def main():
     pygame.init()   # initializing the game
+    c = pygame.time.Clock()
+    dt = 0          # delta is used to represent the amount of time that has passed since the last frame was drawn
     print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     '''
     Video games are generally built using a game loop, the simplest loop has 3 steps
@@ -18,7 +21,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip() #update the contents of the entire display
+        dt = c.tick(60) # pauses game loop until 1/60th of a second has passed also returns the amount of time that has passed since it was last called or delta
+        dt /= 1000 # converting from miliselsconds to seconds
+
+
 
 
 
